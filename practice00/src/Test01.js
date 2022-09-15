91bf0f8403b1c414a2341a641191c6c99f9096c7
inseo <wisberry2022@gmail.com>
2022-09-15 21:08:31 +0900

    09/15 upload

--- practice00/src/Test01.js
+++ practice00/src/Test01.js
@@ -2,18 +2,25 @@ import { useState } from "react";
 
 const Test01 = () => {
   const [info, setInfo] = useState([1, 2, 3]);
-  const [num, setNum] = useState(info[info.length - 1]);
-  // const [num, setNum] = useState(0);
+  const [num, setNum] = useState(4);
 
   const pushInfo = () => {
+    setNum(num + 1);
     setInfo((prevState) => (
       [...prevState, num]
     ))
   }
 
+  const deleteInfo = () => {
+    setInfo(info.filter((it, idx, arr) => idx < arr.length - 1));
+    setNum(info[info.length - 1]);
+    console.log(info);
+  }
+
   return (
     <section className="test">
       <button onClick={() => (pushInfo())}>ADD</button>
+      <button onClick={() => (deleteInfo())}>DELETE</button>
       <ul>
         {info.map((el, idx) => {
           return (

9d8d1478de86e26dcdc0cb09d1c4c73d6824e303
inseo <wisberry2022@gmail.com>
2022-09-19 21:52:11 +0900

    0919 upload

--- practice03/server.js
+++ practice03/server.js
@@ -5,13 +5,15 @@ const axios = require('axios');
 
 const key = 'KcPvvyVEaqORzpTkpeX8%2Bk36SeU1KPEAdUK7gVD%2BKt6hYBONhpq0ZQ8NcPEQR6O%2FuYEdi4c030PNJp5TL66XLw%3D%3D';
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
 app.use(express.static(path.join(__dirname, '/build')));
 
 const currentPut = async () => {
   let response;
   try {
     response = await axios.get(
-      `http://data.humetro.busan.kr/voc/api/open_api_process.tnn?day=1&updown=1&act=json&stime=1350&enum=5&act=json&scode=105&serviceKey=${key}`)
+      `http://data.humetro.busan.kr/voc/api/open_api_process.tnn?serviceKey=${key}&day=1&updown=1&act=json&stime=1350&enum=5&act=json&scode=105`)
   } catch (e) {
     console.log(e)
   }
--- practice03/src/App.js
+++ practice03/src/App.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 function App() {
 
-  axios.get('http://localhost:8080/test').then((response) => {
-    console.log(response);
-  })
+  const key = 'KcPvvyVEaqORzpTkpeX8%2Bk36SeU1KPEAdUK7gVD%2BKt6hYBONhpq0ZQ8NcPEQR6O%2FuYEdi4c030PNJp5TL66XLw%3D%3D';
+
+  axios.get(`/test`)
+    .then((res) => {
+      console.log(res);
+
+    })
 
   return (
     <div className="App">

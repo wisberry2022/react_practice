cbe733cc88367c4d05cb946ed876369d270f7064
wisberry <wisberry2022@gmail.com>
2022-09-30 17:36:01 +0900

    09/30 upload

---
+++ import-export/src/App.js
@@ -0,0 +1,11 @@
+import { a } from './Child01';
+
+function App() {
+  return (
+    <div className="App">
+      {console.log(a)}
+    </div>
+  );
+}
+
+export default App;
---
+++ import-export/src/Child01.js
@@ -0,0 +1,19 @@
+const a = 0;
+const d = 10;
+
+const onTest = () => {
+  console.log('hello');
+}
+
+const Child01 = () => {
+  return (
+    <>
+      Child
+    </>
+  )
+}
+
+// export default Child01;
+export { onTest };
+export const b = 10;
+export default a;
